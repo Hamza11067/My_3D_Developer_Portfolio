2b19f3dd92a3461ca3ddf9690cdb20527fa6e482
Hamza11067 <hamzakhalid1067@gmail.com>
2024-05-07 08:40:20 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React.js + Three.js
+
+This is my 3D developer porfolio.
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,105 @@
+/* eslint-disable react/no-unknown-property */
+import PropTypes from 'prop-types'; // Import PropTypes
+import { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
+  const meshRef = useRef();
+
+  // Use state to hold rotation values
+  const [isHovered, setIsHovered] = useState(false);
+  const [rotation, setRotation] = useState(0);
+
+  // Use useFrame to update rotation on each frame
+  useFrame(() => {
+     // Update rotation only if not hovered
+     if (!isHovered) {
+      setRotation(rotation + 0.005);
+    }
+  });
+
+  return (
+    <mesh ref={meshRef} rotation={[ 0, rotation, 0]} 
+    onPointerOver={() => setIsHovered(true)}
+    onPointerOut={() => setIsHovered(false)}
+    >
+      <hemisphereLight intensity={1.5} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1.5} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.8}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+// these proptype lines are written to avoid "proptype misssing in validation" error
+Computers.propTypes = {
+  isMobile: PropTypes.bool.isRequired, // Specify type as boolean and required
+};
+
+
+const ComputersCanvas = () => {
+  
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
